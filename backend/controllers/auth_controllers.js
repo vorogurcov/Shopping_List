68e2a51911915224623c151f14287f3475f7609c
vorogurcov <vania.frol2010@yandex.ru>
2024-12-17 12:14:12 +0300

    Change controllers.register_user logic to have try-catch block

--- backend/controllers/auth_controllers.js
+++ backend/controllers/auth_controllers.js
@@ -3,24 +3,29 @@ const asyncHandler = require("express-async-handler");
 const pool = require("../config/db.js");
 
 exports.register_user = asyncHandler((req, res) => {
-    let values = Object.values(req.body)
+    try {
+        let values = Object.values(req.body);
 
-    const query = {
-        text: 'INSERT INTO users(user_name, user_last_name, user_email, user_password) VALUES($1, $2, $3, $4)',
-        values: values,
+        const query = {
+            text: 'INSERT INTO users(user_name, user_last_name, user_email, user_password) VALUES($1, $2, $3, $4)',
+            values: values,
+        };
+
+        const result = pool.query(query);
+
+        res.status(200).json({ message: "Registration successful" });
+    } catch (error) {
+        console.error("Error during user registration:", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
     }
-
-    const result = pool.query(query)
-
-    res.status(200).json({ message: "Registration successful" });
-})
+});
 
 exports.signup_user = asyncHandler(async (req, res) => {
     try {
-        // Извлечение значений из тела запроса
+
         let values = Object.values(req.body)
 
-        // Параметризованный SQL-запрос
+
         const query = {
             text: 'SELECT * FROM users WHERE user_email = $1 AND user_password = $2',
             values: values,
@@ -28,10 +33,10 @@ exports.signup_user = asyncHandler(async (req, res) => {
 
         console.log(query);
 
-        // Выполнение запроса
+
         const result = await pool.query(query);
 
-        // Проверка результата
+
         if (result.rows.length > 0) {
             res.status(200).json({ message: "All good!" });
             console.log("All good!");
